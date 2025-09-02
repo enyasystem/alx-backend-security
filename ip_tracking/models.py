d4357389e4d1324867272641265bd9ef0fe9249c
enyasystem <enyaelvis@gmail.com>
2025-09-02 19:14:10 +0100

    Add RequestLog model to track IP addresses and requested paths

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class RequestLog(models.Model):
+    # Stores the IP address of the requester
+    ip_address = models.GenericIPAddressField()
+    # Stores the path that was requested
+    path = models.CharField(max_length=255)
+
+def __str__(self):
+    # Helpful for debugging and thhe admin display
+    return f"{self.ip_address} at {self.timestamp} requested {self.path}"
