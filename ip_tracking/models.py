dbb29656b07788165ad2950727964b1a2cf34ec9
enyasystem <enyaelvis@gmail.com>
2025-09-04 10:18:16 +0100

    Refactor RequestLog model to enforce unique IP addresses and improve string representation

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class RequestLog(models.Model):
     # Stores the IP address of the requester
-    ip_address = models.GenericIPAddressField()
+    ip_address = models.GenericIPAddressField(unique=True)
     # Stores the path that was requested
     path = models.CharField(max_length=255)
 
-def __str__(self):
-    # Helpful for debugging and thhe admin display
-    return f"{self.ip_address} at {self.timestamp} requested {self.path}"
+    def __str__(self):
+        # Helpful for debugging and the admin display
+        return f"{self.ip_address} - {self.path}"
