ebd48bcf7d7c24bc4204c88b173450913f66dfec
enyasystem <enyaelvis@gmail.com>
2025-09-05 10:24:11 +0100

    Refactor settings.py to include database configuration and static file settings

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,17 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
+from pathlib import Path
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 from pathlib import Path
 
